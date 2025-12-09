23babee0077d4059b1920d8b5d64a2a110f573ef
MaJungHan <wjdgksak@naver.com>
2025-12-09 23:04:34 +0900

    feat(post): 제목+작성자 유니크 인덱스 추가 및 중복 생성 에러 핸들링 적용

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -1,6 +1,9 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, Index } from 'typeorm';
 
 @Entity('posts')
+@Index('IDX_POST_TITLE', ['title']) // title의 대한 인덱스 이름 지정 : 더빨리 검색함
+@Index('IDX_POST_AUTHOR_ID', ['authorId']) // authorId 대한 인덱스 이름 지정
+@Index('IDX_POST_TITLE_AUTHOR_ID', ['title', 'authorId'], { unique: true })
 export class Post {
   @PrimaryGeneratedColumn({
     type: 'int',
